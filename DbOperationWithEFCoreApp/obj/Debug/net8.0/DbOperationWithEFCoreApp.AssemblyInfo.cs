2c8f1c58a31e50bd7a3b305467f73f082d28b4ba
Technohubjk <sosshivam65@gmail.com>
2026-01-04 13:14:16 +0530

    Add HomeController and clean Visual Studio files

--- DbOperationWithEFCoreApp/obj/Debug/net8.0/DbOperationWithEFCoreApp.AssemblyInfo.cs
+++ DbOperationWithEFCoreApp/obj/Debug/net8.0/DbOperationWithEFCoreApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DbOperationWithEFCoreApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cda7acb5eb535b25f8371e6a9a63c9481d363387")]
 [assembly: System.Reflection.AssemblyProductAttribute("DbOperationWithEFCoreApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DbOperationWithEFCoreApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
